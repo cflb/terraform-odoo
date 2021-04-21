49131acc1f43defa57cf0389985c2b2b1ce4bc84
cflb <cleyton.flb@gmail.com>
2021-04-20 23:34:47 -0300

    UPDATE: define token and sshkeys vars

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@
 # INFO: OdooServer
 # REPOSITORY: https://github.com/cflb/terraform-odoo
 
+# Define token and ssh key vars
+variable "do_token" {}
+variable "pvt_key" {}
+
+# provider digitalocean - set token - get $ENV_VARS
+provider "digitalocean" {
+    token = var.do_token
+}
+
+# Get ssh key in digitalocean account
+data "digitalocean_ssh_key" "terraform" {
+  name = "terraform"
+}
+
 # Define DigitalOcean provider
 terraform {
     required_providers {
