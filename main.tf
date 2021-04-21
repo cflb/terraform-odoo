59687f62631bfe52d80d5a661940bcd0fd6d432e
cflb <cleyton.flb@gmail.com>
2021-04-20 23:46:06 -0300

    UPDATE: add resource droplet odooServer-01

--- main.tf
+++ main.tf
@@ -24,3 +24,16 @@ terraform {
         } 
     }
 }
+
+# Define Resources
+# OdooServer
+resource "digitalocean_droplet" "odooServer" {
+  image = "ubuntu-20-04-x64"
+  name = "odooServer-01"
+  region = "nyc1"
+  size = "s-1vcpu-1gb"
+  private_networking = true
+  ssh_keys = [ 
+      data.digitalocean_ssh_key.terraform.id
+   ]
+}
