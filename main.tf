c589c349d2ec408f477babb5d4c0d90ee3fe7dfd
cflb <cleyton.flb@gmail.com>
2021-04-22 14:25:12 -0300

    FIX: add var in varfile and add ansible provisioner parans

--- main.tf
+++ main.tf
@@ -2,40 +2,48 @@
 # INFO: OdooServer
 # REPOSITORY: https://github.com/cflb/terraform-odoo
 
-# Define token and ssh key vars
-variable "do_token" {}
-# variable "pvt_key" {}
-
 # provider digitalocean - set token - get $ENV_VARS
 provider "digitalocean" {
-    token = var.do_token
+  token = var.do_token
 }
 
 # Get ssh key in digitalocean account
 data "digitalocean_ssh_key" "do-sshkey-name" {
-    name = "do-sshkey-name"
+  name = "do-sshkey-name"
 }
 
 # Define DigitalOcean provider
 terraform {
-    required_providers {
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-        } 
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
     }
+  }
 }
 
 # Define Resources
 # An Odoo Application will be installed: OdooServer
 resource "digitalocean_droplet" "OdooServer" {
-  image = "ubuntu-20-04-x64"
-  name = "odooServer-01"
-  region = "nyc1"
-  size = "s-1vcpu-1gb"
+  image              = "ubuntu-20-04-x64"
+  name               = "odooServer-01"
+  region             = "nyc1"
+  size               = "s-1vcpu-1gb"
   private_networking = true
-  ssh_keys = [ 
-      data.digitalocean_ssh_key.do-sshkey-name.id
-   ]
+  ssh_keys = [
+    data.digitalocean_ssh_key.do-sshkey-name.id
+  ]
+
+  provisioner "remote-exec" {
+
+    inline = ["apt update", "sudo apt install python3 -y", "echo Done!"]
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      host        = self.ipv4_address
+      private_key  = var.pvt_key
+    }
+  }
 
   lifecycle {
     create_before_destroy = true
@@ -43,8 +51,4 @@ resource "digitalocean_droplet" "OdooServer" {
       ssh_keys,
     ]
   }
-}
-
-output "ip_do_droplet" {
-  value = digitalocean_droplet.OdooServer.ipv4_address
 }
